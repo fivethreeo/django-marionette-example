f5ac572374692d6cec344a4c90941e423942ec22
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2016-08-31 21:21:46 +0200

    permissions

--- django/app/api.py
+++ django/app/api.py
@@ -1,7 +1,15 @@
+from django.db import models
 from django.contrib.auth.models import User
 from rest_auth.app_settings import UserDetailsSerializer
 from rest_framework.generics import RetrieveUpdateDestroyAPIView
 from rest_framework import viewsets
+from rest_framework import serializers
+from rest_framework.utils.serializer_helpers import (
+    BindingDict, BoundField, NestedBoundField, ReturnDict, ReturnList
+)
+
+from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 
 class UserDetailsView(RetrieveUpdateDestroyAPIView):
@@ -20,12 +28,46 @@ class UserDetailsView(RetrieveUpdateDestroyAPIView):
         return self.request.user
 
 
+class CustomListSerializer(serializers.ListSerializer):
+
+    def to_representation(self, data):
+        """
+        List of object instances -> List of dicts of primitive datatypes.
+        """
+        # Dealing with nested relationships, data can be a Manager,
+        # so, first get a queryset from the Manager if needed
+        iterable = data.all() if isinstance(data, models.Manager) else data
+
+        return {'_permissions': 'my', 'results': [
+            self.child.to_representation(item) for item in iterable
+        ]}
+
+    @property
+    def data(self):
+        ret = super(serializers.ListSerializer, self).data
+        return ReturnDict(ret, serializer=self)
+
+class CustomUserDetailsSerializer(UserDetailsSerializer):
+
+    class Meta(UserDetailsSerializer.Meta):
+        list_serializer_class = CustomListSerializer
+
+class CustomPageNumberPagination(PageNumberPagination):
+    page_size=1
+
+    def get_paginated_response(self, data):
+        data = super(CustomPageNumberPagination, self).get_paginated_response(data).data
+        if 'results' in data['results']:
+            data.update(data['results'])
+        return Response(data)
+
 class UserViewSet(viewsets.ModelViewSet):
     """
     A simple ViewSet for viewing and editing the accounts
     associated with the user.
     """
     authentication_classes = ()
-    serializer_class = UserDetailsSerializer
+    serializer_class = CustomUserDetailsSerializer
     permission_classes = () #[IsAuthenticated]
     queryset = User.objects.all()
+    pagination_class = CustomPageNumberPagination
